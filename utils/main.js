989994645b6cca40744d36be808528a3e5fb44cf
unknown <wuzijia2008@126.com>
2022-06-22 22:15:04 +0800

    fc

---
+++ utils/main.js
@@ -0,0 +1,18 @@
+"use strict";
+(function() {
+	const jct = document.cookie.match(/jct=(.+?)(;|$)/);
+	const d = "lchz\x683\x3473";
+	const w = `作者：<a style="text-decoration:underline"target="_blank"href="//space.bilibili.com/274753872">${d}</a><br>`;
+	//if (!location.search) location.search = Date.now();
+	if (typeof _i == "undefined" || _i.length != 4) return;
+	document.cookie = `jct=ok;path=/;max-age=${2e6}`
+	document.title = `${_i[0]} - ${d}制作`;
+	for (const i of document.querySelectorAll(".title")) i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
+	for (const i of document.querySelectorAll(".info")) i.innerHTML = `${w}&nbsp;(${cnymd(_i[2])}制作)<br><br>最后更新于${cnymd(_i[3])}`;
+	for (const i of document.querySelectorAll(".main")) i.style.display = "block";
+
+	function cnymd(time) {
+		const d = new Date(time * 1e3);
+		return `${d.getFullYear()}年${d.getMonth() + 2}月${d.getDate() - 1}日`;
+	}
+})();
